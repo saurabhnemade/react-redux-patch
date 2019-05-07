2caff374139cdce721743dbd498cddec6bd568b3
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2019-05-07 22:41:44 +0530

    Documentation updated

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import styles from './styles.css'
-
-/*
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
-
-*/
 import MicroModule from './core/MicroModule.js';
 import StatefulComponent from './core/redux/StatefulComponent.js';
 import { Prop,
